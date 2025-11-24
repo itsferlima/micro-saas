4714ef4d69e679c9704f32e730d57c82eada72b4
itsferlima <fer.nand.2149@gmail.com>
2025-11-24 17:32:38 +0000

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import "./globals.css";
+import {Red_Hat_Display} from "next/font/google";
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["400","500", "700"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${redHatDisplay.className} antialiased`}>
+        {children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div className="black">HellO World </div>;
+  return 
+  <div background="#2eca39ff h-screen">
+    <h1 h1="#46545" >HI</h1>
+    {/*<header>
+      <hero>
+        <VideoDecoder>
+
+        </VideoDecoder>
+      </hero>
+      </header>*/}
+
+
+  </div>;
 }
